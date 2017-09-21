2f2b3de91e8fb98ea2ec2c0d33f5568b524823af
mrb255 <mrb255@users.noreply.github.com>
2017-09-21 13:32:29 -0800

    Add files via upload

---
+++ File1.cpp
@@ -0,0 +1,23 @@
+/*
+ * File1.cpp
+ *
+ *  Created on: Sep 21, 2017
+ *      Author: matthew
+ */
+
+#include "File1.h"
+//#include "FileA.h"
+#include <iostream>
+
+void FooZ(const std::string &str)
+{
+	std::cout << FooZInternal(str) << std::endl;
+	//std::cout << FooA(str) << std::endl;
+}
+
+std::string FooZInternal(const std::string &str)
+{
+	if(str.length() == 0) return "";
+
+	return str[0] + std::string("?") + FooZ(str.substr(1));
+}
---
+++ File1.h
@@ -0,0 +1,16 @@
+/*
+ * File1.h
+ *
+ *  Created on: Sep 21, 2017
+ *      Author: matthew
+ */
+
+#ifndef FILE1_H_
+#define FILE1_H_
+
+#include <string>
+
+void FooZ(const std::string &str);
+
+
+#endif /* FILE1_H_ */
