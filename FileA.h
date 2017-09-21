d98a40adc6671f293596f0091836a53a98c59206
sjcrawford <32178974+sjcrawford@users.noreply.github.com>
2017-09-21 14:02:28 -0800

    Add files via upload

--- FileA.h
+++ FileA.h
@@ -1,1 +1,16 @@
-//
+#ifndef FILEA_H
+#define FILEA_H
+
+#include <string.h>
+#include <iostream>
+#include "File1.h"
+
+using namespace std;
+
+string FooAInternal(string input);
+string FooA(string input);
+
+}
+
+
+#endif
